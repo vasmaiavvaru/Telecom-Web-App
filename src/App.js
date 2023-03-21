cfb8414ac3512d1187b7720fedad8d6c73a7b6dd
vasmai <avvaru.vasmai@stl.tech>
2023-03-21 14:55:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Form, Button, Input, Label } from "react-dom";
+import { BrowserRouter  , Routes, Route} from 'react-router-dom';
 import { HomePage, LoginPage, CustomerSignupPage, MyAccountPage, CustomerProfilePage } from './pages';
-import { Button } from './Button';
+
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <div >
+      <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<HomePage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/customersignup" element={<CustomerSignupPage />}></Route>
-          <Route path="/myaccount" element={<MyAccountPage />}></Route>
-          <Route path="/customerprofile" element={<CustomerProfilePage />}></Route>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/signup" element={<CustomerSignupPage/>} />
         </Routes>
-      </Router>
-
+    </BrowserRouter>
     </div>
   );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,50 @@
-import React from 'react';
+import React from "react";
 
-export const LoginPage = () => (
-    <h1>LOGIN PAGE</h1>
-    
-);
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardImg
+} from "reactstrap";
+import { GoogleLoginButton } from "react-social-login-buttons";
+
+export const  LoginPage = ()=> (
+  <div className="background">
+    <div className="login-box">
+      <div className="container">
+        <div class="row app-des">
+          <div class="col left-background ">
+            <h2>LOGIN</h2>
+            <p>Powered by Vasmai</p>
+            
+          </div>
+          <div class="col login-form" >
+            <form >
+              <h2 className="font-weight-bold mb-4">Customer Login</h2>
+              <FormGroup>
+                <Label className="font-weight-bold mb-2">Email</Label> <br/><br/>
+                <Input
+                  className="mb-3"
+                  type="email"
+                  placeholder="John@example.com" /><br/>
+                <Label className="font-weight-bold mb-2">Password</Label><br/><br/>
+                <Input
+                  className="mb-3"
+                  type="password"
+                  placeholder="At least 8 characters" />
+              </FormGroup>
+              <Button className="mt-3  btn">Login to your account</Button><br/><br/>
+              {/* <div className="text-center m-4">or continue with social account</div>
+      <GoogleLoginButton className="mt-3 mb-3 px-auto text-center"/> */}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+
